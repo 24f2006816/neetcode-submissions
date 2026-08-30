48243852f52f30a8ee6244cedefb962217fb2539
Pratyaksh Pandey <232079021+24f2006816@users.noreply.github.com>
2026-08-31 00:40:27 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i=0; i <n; i++){
+            minPrice = min(minPrice, prices[i]);
+            int profit = prices[i] - minPrice;
+            maxProfit = max(maxProfit, profit);
+        }
+        return maxProfit;
+    }
+};
