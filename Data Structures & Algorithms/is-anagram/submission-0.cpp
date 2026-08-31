23fbca9651c4a9cc93589f7087f3146653a05130
Pratyaksh Pandey <232079021+24f2006816@users.noreply.github.com>
2026-08-31 08:01:52 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        int count[26] = {};
+        for (char c: s) count[c-'a']++;
+        for (char c: t) count[c-'a']--;
+        for (int i: count) if (i != 0) return false;
+        return true;
+    }
+};
